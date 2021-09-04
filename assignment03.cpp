26cbb45e41f389f9d79ae726c086c3f0a03d9697
wanttosleep <88647945+WeerawanV@users.noreply.github.com>
2021-09-05 01:19:08 +0700

    Add files via upload

---
+++ assignment03.cpp
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<Windows.h>
+#include<conio.h>
+
+void gotoxy(int x, int y) {
+	COORD c = { x,y };
+	SetConsoleCursorPosition(
+		GetStdHandle(STD_OUTPUT_HANDLE), c);
+}
+
+void draw_ship() {
+	printf(" <-0-> ");
+}
+
+void draw_ship(int x, int y) {
+	gotoxy(x, y);
+	draw_ship();
+}
+
+int main() {
+	char ch = ' ';
+	int x = 38, y = 20;
+	draw_ship(x, y);
+	do {
+		if (_kbhit()) {
+			ch = _getch();
+			if (ch == 'a' && x >= 1) { 
+				draw_ship(--x, y);
+			}
+			if (ch == 'd' && x <= 80) {
+				draw_ship(++x, y);
+			}
+			fflush(stdin);
+		}
+		Sleep(100);
+	} while (ch != 'x');
+	return 0;
+}
